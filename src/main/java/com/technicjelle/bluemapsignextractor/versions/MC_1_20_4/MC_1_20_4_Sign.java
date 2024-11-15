23a9773c21f3d7eb9a0004e407873326c5bc1411
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-11-16 00:04:25 +0100

    Add & improve some comments

--- src/main/java/com/technicjelle/bluemapsignextractor/versions/MC_1_20_4/MC_1_20_4_Sign.java
+++ src/main/java/com/technicjelle/bluemapsignextractor/versions/MC_1_20_4/MC_1_20_4_Sign.java
@@ -3,8 +3,8 @@ package com.technicjelle.bluemapsignextractor.versions.MC_1_20_4;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.technicjelle.bluemapsignextractor.common.BlockEntity;
-import com.technicjelle.bluemapsignextractor.common.SignColour;
 import com.technicjelle.bluemapsignextractor.common.HTMLUtils;
+import com.technicjelle.bluemapsignextractor.common.SignColour;
 import com.technicjelle.bluemapsignextractor.versions.MC_1_13_2.MC_1_13_2_Sign;
 import de.bluecolored.bluenbt.NBTName;
 import org.jetbrains.annotations.NotNull;
@@ -22,15 +22,22 @@ public class MC_1_20_4_Sign extends BlockEntity {
 		private boolean isGlowing;
 
 		public static String unJSON(String text) {
-			//TODO: This text might have extra JSON in it, like colour overrides. Currently we just ignore everything but the text, but perhaps we should parse it properly.
-			// Test `test_MC_1_20_4_SignWithCustomJSON` has an example of this.
+			//Remove surrounding quotation marks and handle escaped characters
 			try {
 				final String key = "text";
 				final JsonParser parser = new JsonParser();
 
+				//TODO: This text might have extra JSON in it, like colour overrides.
+				// Currently we just ignore everything but the text,
+				// but perhaps we should parse it properly and return the coloured text.
+				// Test `test_MC_1_20_4_SignWithCustomJSON` has an example of this.
 				final JsonObject o = parser.parse("{\"" + key + "\":" + text + "}").getAsJsonObject();
 				return o.get(key).getAsString();
 			} catch (UnsupportedOperationException e) {
+				//If the text could not be parsed by wrapping it in JSON, then it might BE a JSON already.
+				//Which means it likely has colour codes or other formatting.
+				//We can use 1.13's parser for that.
+				//TODO: However, in some rare cases, the JSON might not even HAVE a "text" key, so we need to catch that.
 				return MC_1_13_2_Sign.unJSON(text);
 			}
 		}
