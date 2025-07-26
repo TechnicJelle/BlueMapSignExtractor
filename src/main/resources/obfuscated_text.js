3b1f14c8fc133747da1194e86364d955646ac39b
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-07-26 03:29:51 +0200

    Do not obfuscate sign text if the sign isn't even visible

--- src/main/resources/obfuscated_text.js
+++ src/main/resources/obfuscated_text.js
@@ -3,7 +3,9 @@ const obfuscationCharactersLength = obfuscationCharacters.length;
 
 function doSignObfuscation() {
 	for (const el of document.getElementsByClassName("obfuscated")) {
+		if (!el.checkVisibility()) continue;
 		const length = el.innerHTML.length;
+		if (length === 0) continue;
 		let randomText = "";
 		for (let i = 0; i < length; i++) {
 			randomText += obfuscationCharacters.charAt(Math.floor(Math.random() * obfuscationCharactersLength));
